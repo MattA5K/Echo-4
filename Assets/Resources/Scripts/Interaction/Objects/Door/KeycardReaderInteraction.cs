26cd7640f1ee6048ae144d47344247b9ab08d4fe
MattA5K <mattask.gthb@gmail.com>
2026-04-09 15:31:47 +0700

    Commit 4 : Keycard mechanics
    + Foundations for keycard art and item slots

--- Assets/Resources/Scripts/Interaction/Objects/Door/KeycardReaderInteraction.cs
+++ Assets/Resources/Scripts/Interaction/Objects/Door/KeycardReaderInteraction.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class KeycardReaderInteraction : MonoBehaviour, IInteractable
 {
     public bool isInteractable = true; // placeholder
-    
+
+    public ItemData acceptedKeycard;
     public void Interact()
     {
-
+        KeycardReaderUIManager.Instance.ShowReaderPanel(this);
     }
 
     public bool CanInteract()
--- Assets/Resources/Scripts/UI/KeycardReaderUIManager.cs
+++ Assets/Resources/Scripts/UI/KeycardReaderUIManager.cs
@@ -4,18 +4,26 @@ using UnityEngine.UIElements;
 
 public class KeycardReaderUIManager : MonoBehaviour
 {
+    public static KeycardReaderUIManager Instance;
+    private KeycardReaderInteraction currentReader;
     private ItemData slotItemData;
 
     [SerializeField] private GameObject readerPanel;
     [SerializeField] private Image itemIcon;
     [SerializeField] private TMP_Text text;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     public void ShowReaderPanel(KeycardReaderInteraction reader)
     {
-        
+        currentReader = reader;
+        readerPanel.SetActive(true);
     }
 
     public void HideReaderPanel()
     {
-
+        readerPanel.SetActive(false);
     }
 }
